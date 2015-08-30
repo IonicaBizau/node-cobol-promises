db26ea9f5144a65f71f954a07681399bf3d11dbf
Ionică Bizău <bizauionica@yahoo.com>
2015-08-30 21:48:13 +0300

    Promises support

--- example/index.js
+++ example/index.js
@@ -1,80 +1,9 @@
 // Dependencies
-var Cobol = require("../lib");
+var Cobol = require("../lib")
+  , Promise = require("promiscuous")
+  ;
 
-// Execute some COBOL snippets
-Cobol(function () { /*
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLO.
-       ENVIRONMENT DIVISION.
-       DATA DIVISION.
-       PROCEDURE DIVISION.
-
-       PROGRAM-BEGIN.
-           DISPLAY "Hello world".
-
-       PROGRAM-DONE.
-           STOP RUN.
-*/ }, function (err, data) {
-    console.log(err || data);
-});
-// => "Hello World"
-
-Cobol(__dirname + "/args.cbl", {
-    args: ["Alice"]
-}, function (err, data) {
-    console.log(err || data);
-});
-// => "Your name is: Alice"
-
-
-// This will read data from stdin
-Cobol(function () { /*
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. APP.
-      *> http://stackoverflow.com/q/938760/1420197
-
-       ENVIRONMENT DIVISION.
-       INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-       SELECT SYSIN ASSIGN TO KEYBOARD ORGANIZATION LINE SEQUENTIAL.
-
-       DATA DIVISION.
-       FILE SECTION.
-       FD SYSIN.
-       01 ln PIC X(64).
-          88 EOF VALUE HIGH-VALUES.
-       WORKING-STORAGE SECTION.
-       PROCEDURE DIVISION.
-       DISPLAY "Write something and then press the <Enter> key"
-       OPEN INPUT SYSIN
-       READ SYSIN
-       AT END SET EOF TO TRUE
-       END-READ
-       PERFORM UNTIL EOF
-       DISPLAY "You wrote: ", ln
-       DISPLAY "------------"
-       READ SYSIN
-       AT END SET EOF TO TRUE
-       END-READ
-       END-PERFORM
-       CLOSE SYSIN
-       STOP RUN.
-*/ }, {
-    stdin: process.stdin
-  , stdout: process.stdout
-}, function (err) {
-    if (err) {
-        console.log(err);
-    }
-});
-// => Write something and then press the <Enter> key
-// <= Hi there!
-// => You wrote: Hi there!
-// => ------------
-
-//you can also use our promise based API
-
-var Q = require('q');
+// Create a hello world
 var helloWorld1 = Cobol(function () { /*
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
@@ -87,6 +16,7 @@ var helloWorld1 = Cobol(function () { /*
        STOP RUN.
  */ });
 
+// Create another hello world
 var helloWorld2 = Cobol(function () { /*
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
@@ -99,9 +29,10 @@ var helloWorld2 = Cobol(function () { /*
        STOP RUN.
  */ });
 
-Q.all([helloWorld1, helloWorld2]).then(function (res) {
+Promise.all([helloWorld1, helloWorld2]).then(function (res) {
     console.log(res);
 }).catch(function(err) {
     console.log("Something bad happened!" + err);
 });
-// => [ 'Hello world', 'Hello world second time!' ]
+// => Hello world
+// => Hello world second time!
